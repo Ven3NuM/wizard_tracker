5cdaf301f0baddbceb7402dd90eeae2fd9b967df
thmarantos <thmarantos@gmail.com>
2019-11-18 15:42:44 +0200

    Homepage: CSS added inputs/buttons, Homepage: Missing Func

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import './styles/main.scss';
 import { localhost } from './messages';
 
 console.log(localhost());
+
+//var nopHTMLCollection = document.getElementsByClassName('wrapper__players');
+//var numOfPlayers = Array.from(nopHTMLCollection);
+//var numOfPlayers__btn = numOfPlayers[0].getElementsByClassName('btn');
